27ce8b58bf62c7eadc62dcafe59fb5e22d814d6b
Tom hoffman <tomhoffmanco@gmail.com>
2022-08-14 17:34:18 -0400

    still trying to get heroku to deploy

--- server.js
+++ server.js
@@ -17,12 +17,15 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(require("./routes/api.js"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/BudgetTracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
